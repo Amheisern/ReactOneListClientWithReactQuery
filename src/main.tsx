15fddeff6a83d27098b9f50a901e8bd04a61442e
lodashreload <icarusjapan@gmail.com>
2022-04-24 16:59:33 -0400

    added configured react quiery

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import { App } from './App'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { BrowserRouter as Router } from 'react-router-dom'
 
+
+
+const queryClient = new QueryClient()
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
